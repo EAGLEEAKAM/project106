f8a6a1263f52591232101bc511ccccd084e8b4c4
EAGLEEAKAM <77165496+EAGLEEAKAM@users.noreply.github.com>
2022-02-03 21:12:05 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+function startclassification(){
+    navigator.mediaDevices.getUserMedia({audio:true});
+    classifier=ml5.soundClassifier('model.json',modelReady);
+    }
+    
+    function modelReady(){
+        classifier.classify(gotResult);
+    }
